79071b8b6615f43e016a411144b8cdb6d7033ce5
Adnan Farra <62692354+AdnanFarra1@users.noreply.github.com>
2020-05-21 10:16:30 +0300

    Added CSProject VBFiles

---
+++ CSProject/Main_Program/Main_Program/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+    Dim NParticipants As Integer
+
+    Structure Participants_Structure
+        Dim ID As Integer
+        Dim Full_Name As String
+        Dim DOB As Date
+    End Structure
+
+    Sub Main()
+        Console.WriteLine("Please enter the number of participants.")
+        NParticipants = Console.ReadLine
+
+        Dim Participants(0 To NParticipants) As Participants_Structure
+        For i = 0 To NParticipants
+            Console.WriteLine("Please enter participant ID")
+            Participants(i).ID = Console.ReadLine
+            Console.WriteLine("Please enter participant full name")
+            Participants(i).Full_Name = Console.ReadLine
+            Console.WriteLine("Please enter participant DOB")
+            Participants(i).DOB = Console.ReadLine
+        Next
+        Console.WriteLine(Participants(2))
+
+
+
+
+
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
