7a752ae2113ad7376ce196746227c6952639680e
Adnan Farra <adnanfarra1@gmail.com>
2020-09-10 08:29:22 +0300

    Added numpy library, added menu

---
+++ Stacks/Stacks.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print("Choose one of the following options:")
+print("1) Initialise Stack")
+print("2) Add a new item to the stack")
+print("3) Remove an item from the stack")
+print("4) Display the list")
+print("5) Exit")
+
+x = np.array
