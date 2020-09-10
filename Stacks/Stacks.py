932504fbb021d4a51fe67e274585f24efaf0f139
Adnan Farra <adnanfarra1@gmail.com>
2020-09-10 08:59:56 +0300

    If statement for menu, added function InitStack

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -7,4 +7,24 @@ print("3) Remove an item from the stack")
 print("4) Display the list")
 print("5) Exit")
 
-x = np.array
+choice = int(input("\nPlease choose an option from 1-5\n"))
+if choice ==1:
+    print("you chose 1")
+elif choice == 2:
+    print("You chose 2")
+elif choice == 3:
+    print("You chose 3")
+elif choice == 4:
+    print("you choose 4")
+elif choice == 5:
+    print("you chose 5")
+else:
+    print("Please choose an item from the menu above")
+
+
+def Initisialise_Stack():
+    BaseOfStack = 0
+    TopOfStack = -1
+
+
+###x = np.array
