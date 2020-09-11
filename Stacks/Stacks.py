e6f88478d111195efad5f547ebc261c482b040cb
Adnan Farra <adnanfarra1@gmail.com>
2020-09-11 10:51:49 +0300

    Configured initialise stack

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -1,15 +1,22 @@
 import numpy as np
 
+def Initisialise_Stack():
+    BaseOfStack = 0
+    TopOfStack = -1
+    stack = np.full((4,1),"A")
+    print(stack)
+    print("Stack Initialised")
+
+
 print("Choose one of the following options:")
 print("1) Initialise Stack")
 print("2) Add a new item to the stack")
 print("3) Remove an item from the stack")
 print("4) Display the list")
 print("5) Exit")
-
 choice = int(input("\nPlease choose an option from 1-5\n"))
-if choice ==1:
-    print("you chose 1")
+if choice == 1:
+    Initisialise_Stack()
 elif choice == 2:
     print("You chose 2")
 elif choice == 3:
@@ -17,14 +24,14 @@ elif choice == 3:
 elif choice == 4:
     print("you choose 4")
 elif choice == 5:
+    print("Exiting Program...")
     quit()
 else:
     print("Please choose an item from the menu above")
 
 
-def Initisialise_Stack():
-    BaseOfStack = 0
-    TopOfStack = -1
 
 
-###x = np.array
+
+
+
