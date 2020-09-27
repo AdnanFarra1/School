90a14e1380e082cbc600bb19fec1a5c69abfad89
Adnan Farra <62692354+AdnanFarra1@users.noreply.github.com>
2020-09-27 09:47:28 +0300

    Update Task_1.2.py
    Added is_found variable and used it to display item is not found

--- Pre-Release_Material/Task_1.2.py
+++ Pre-Release_Material/Task_1.2.py
@@ -1,11 +1,18 @@
 #Searching for multiple instaces
 ItemDescription = [1,2,3,4,5,9,7,8,9,10]
+is_found = False
 
 search_value = int(input("Please input a search value: "))
 for i in range(0,9):
    
     if ItemDescription[i] == search_value:
         print("The index of search value: ", i)
+        is_found = True
+    elif i == 9 & is_found == False:
+        print("The item is not found")
+
+
+    
 
 
 
