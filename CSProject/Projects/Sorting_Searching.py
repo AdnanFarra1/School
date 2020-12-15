ca4ddac4cc444a4361760df4103ec28b8ea4f30b
Adnan Farra <62692354+AdnanFarra1@users.noreply.github.com>
2020-12-15 10:14:40 +0200

    Added Comments to explain code

--- CSProject/Projects/Sorting_Searching.py
+++ CSProject/Projects/Sorting_Searching.py
@@ -1,10 +1,11 @@
 class Student:
+    #User defined data type, in python it is called a class, same as a structure or record
 
     def __init__(self, Student_ID, Student_Name, Student_Score):
         self.Student_ID = Student_ID
         self.Student_Name = Student_Name
         self.Student_Score = Student_Score
-
+    #Class method to ask user for data entry
     @classmethod
     def from_input(cls):
         return cls(
@@ -15,14 +16,13 @@ class Student:
 Students = []
 def Score_Entry():
     No_Scores = int(input("How many scores would you like to enter? "))
+    #For loop to store user inputs in an array, list in python
     for i in range(No_Scores):
         Students.append(Student.from_input())
-    print(Students[0].Student_ID)
-    print(Students[0].Student_Name)
-    print(Students[0].Student_Score, "M")
-    #print("Student ID entered is: ",Stodent.Student_ID)
-    #print("Student Name entered is: ",Stodent.Student_Name)
-    #print("Student Score entered is: ",Stodent.Student_Score, "M")
+    #Data Entry Checks
+    #print(Students[0].Student_ID)
+    #print(Students[0].Student_Name)
+    #print(Students[0].Student_Score, "M")
 def BubbleSort(Students):
     indexing_length = len(Students) - 1
     sorted = False
@@ -32,6 +32,7 @@ def BubbleSort(Students):
         for i in range(0, indexing_length):
             if Students[i].Student_Score > Students[i+1].Student_Score:
                 sorted = False
+                #Swaps index values together if i value is greater than i + 1 value
                 Students[i].Student_Score, Students[i+1].Student_Score = Students[i+1].Student_Score, Students[i].Student_Score
     return Students
 
@@ -44,19 +45,20 @@ print("1) Enter Student Scores")
 print("2) End the program")
 choice = int(input("Please choose an option from the list above: "))
 if choice == 1:
+    #Calling user defined data type data entry function
     Score_Entry()
+    #Menu Choices
     print("1) Bubble Sort")
     print("2) Insertion Sort")
+    #Menu Choices Validated to integer only as python takes any value entered as a string
     choice_2 = int(input("Please choose an option from the list above: "))
     if choice_2 == 1:
+        #Calling BubbleSort function
         BubbleSort(Students)
         for i in range(len(Students)):
             print("Student ID: ",Students[i].Student_ID)
             print("Student Name: ",Students[i].Student_Name)
-            print("Student Score: ",(Students[i].Student_Score))
-
-
-
+            print("Student Score: ",Students[i].Student_Score)
     elif choice_2 == 2:
         InsertionSort()
 
